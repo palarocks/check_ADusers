ccc3652e0321ffa9921dfca209b21ea2c1767f10
Alex <originalpala@gmail.com>
2017-08-12 11:44:29 +0200

    first commit

---
+++ check_ad_users.ps1
@@ -0,0 +1,5 @@
+﻿echo "Check if users are Domain Admin"
+foreach($line in Get-Content .\users.txt) {
+    [bool]$IsDomainAdmin = (Get-ADUser $line -Properties memberof).memberof -contains (Get-ADGroup "Domain Admins") 
+    echo "$line: $IsDomainAdmin"
+}
